050be053f194fa84f17495bc0950151ab723f04c
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-25 19:13:08 +0200

    Change scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//scripts.js
+var x = prompt('Ile poziomów');
+
+function drawTree(x) {
+	return "*";
+	for (i=0, i<x; i++){
+		var star = '',
+		for ( j=0, j<=i; j++){
+			star += '*';
+		}
+
+		}
+	}
+
+}
+console.log(star);
+
+
